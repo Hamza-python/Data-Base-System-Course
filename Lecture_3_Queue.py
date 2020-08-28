50709d643d183c40757300402c13d2508360aec9
Hamza-python <hamzabhatti860@gmail.com>
2020-08-28 11:45:25 +0500

    Code of Queue

---
+++ Lecture_3_Queue.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+list = deque()
+list.appendleft("Mango")
+list.appendleft("Banana")
+list.appendleft("Strawberry")
+list.appendleft("Apple")
+list.appendleft("Cherry")
+list.appendleft("Butter")
+print(list)
+
+list.pop()
+print(list)
+
+list.pop()
+print(list)
+
+list.pop()
+print(list)
+
+list.pop()
+print(list)
+
+list.pop()
+print(list)
